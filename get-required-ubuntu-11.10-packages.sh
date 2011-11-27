f994013fb68d52f88b44f2315fba3fdcdb78eb63
Hans Fangohr [Virtual Machine on MBP13,2011] <fangohr@soton.ac.uk>
2011-11-27 00:00:44 +0000

    minor corrections (adding missing packages)

--- get-required-ubuntu-11.10-packages.sh
+++ get-required-ubuntu-11.10-packages.sh
@@ -2,6 +2,7 @@
 #sudo aptitude install g++ libblas-dev libreadline-dev make m4 gawk zlib1g-dev readline-common liblapack-dev
 
 #to compile using ubuntu's packages
-sudo aptitude install libpetsc3.1-dev libpetsc3.1 libfindlib-ocaml-dev libqhull-dev python-tables python-ply 
-#packages required to compile documentation
+echo "About to install packages required to compile Nmag"
+sudo aptitude install libpetsc3.1-dev libpetsc3.1 libfindlib-ocaml-dev libqhull-dev python-tables python-ply libparmetis-dev python-dev python-py
+echo "About to install packages required to compile documentation"
 sudo aptitude install python-sphinx dvipng texlive-latex-extra texlive-fonts-recommended

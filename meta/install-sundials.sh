7cbc24a6406b7a560c5552f0b218728b7788b7e3
Maximilian Albert <maximilian.albert@soton.ac.uk>
2013-03-07 17:05:50 +0000

    Explain how to uninstall sundials when asking for confirmation to install it in /usr/local.

--- meta/install-sundials.sh
+++ meta/install-sundials.sh
@@ -6,6 +6,7 @@ cd sundials-2.3.0
 ./configure --prefix=/usr/local --enable-shared
 make && 
 echo "To install sundials in /usr/local, press return to run 'sudo make install' or CTRL+C to interrupt"
+echo "(Sundials can later be uninstalled by running 'sudo make uninstall' from this directory)."
 read
 sudo make install
 cd ../..
